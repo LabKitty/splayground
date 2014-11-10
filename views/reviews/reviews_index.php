40c81587abc64be7dc469207eb6edaa17954ef34
LabKitty <labkittyx@gmail.com>
2014-11-10 21:34:09 +0200

    Update review for foreach.

--- views/reviews/reviews_index.php
+++ views/reviews/reviews_index.php
@@ -12,24 +12,22 @@
         <li class="list-group-item">LATEST NEWS</li>
     </ul>
 
-    <?foreach( $news as $new):?>
-    <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-            <img src="<?= ($new['new_pic']) ?>" alt="...">
-            <div class="caption">
-                <h3> <?=$new['new_title']?></h3>
-                <p><?=$new['new_text']?></p>
+    <?foreach( $news as $new):?><!-- Latest news posts -->
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="<?= ($new['new_pic']) ?>" alt="...">
+                <div class="caption">
+                    <h3> <?=$new['new_title']?></h3>
+                    <p><?=$new['new_text']?></p>
 
+                </div>
             </div>
+
         </div>
-
-    </div>
-    <? endforeach ?><!-- Latest review posts -->
-
-
+    <? endforeach ?><!-- Latest news posts -->
 </div>
 
-<div class=".col-xs-6 .col-md-4">
+<div class=".col-xs-6 .col-md-4"><!-- Latest sidebar  -->
     <div class="pull-right">
         <div class="thumbnail">
             <img src="images/tumb.PNG" alt="..." class="img-circle">
@@ -44,42 +42,25 @@
             </div>
         </div>
     </div>
-</div>
+</div><!-- Latest sidebar  -->
 
 <div class="col-md-8 .col-md-4">
 
-    <ul class="list-group">
-        <li class="list-group-item">ALL REVIEWS </li>
-    </ul>
+<ul class="list-group">
+    <li class="list-group-item">ALL REVIEWS </li>
+</ul>
 
+<?foreach( $reviews as $review):?><!-- Latest review posts -->
     <div class="row">
         <div class="media">
             <a class="media-left media-middle" href="#">
-                <img src="images/tumb.PNG" alt="...">
+                 <img src="<?= ($review['review_pic']) ?>" alt="...">
             </a>
-            <div class="media-body">
-                <h4 class="media-heading">Middle aligned media</h4>
-                ...
-            </div>
-        </div>
 
-        <div class="media">
-            <a class="media-left media-middle" href="#">
-                <img src="images/tumb.PNG" alt="...">
-            </a>
             <div class="media-body">
-                <h4 class="media-heading">Middle aligned media</h4>
-                ...
-            </div>
-        </div>
-
-        <div class="media">
-            <a class="media-left media-middle" href="#">
-                <img src="images/tumb.PNG" alt="...">
-            </a>
-            <div class="media-body">
-                <h4 class="media-heading">Middle aligned media</h4>
-                ...
+                    <h4> <?=$review['review_title']?></h4>
+                     <p><?=$review['review_text']?></p>
             </div>
         </div>
     </div>
+<? endforeach ?><!-- Latest review posts -->
