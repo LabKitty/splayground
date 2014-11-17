4a3868b844e7e2d3812693f7188ffe19c336d66b
LabKitty <labkittyx@gmail.com>
2014-11-17 09:52:02 +0200

    Remove Popular news

--- views/news/news_index.php
+++ views/news/news_index.php
@@ -7,42 +7,6 @@
     </div>
 </nav>
 
-<div class="col-md-8 .col-md-4">
-    <ul class="list-group">
-        <li class="list-group-item">POPULAR NEWS</li>
-    </ul>
-    <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-            <img src="images/tumb.PNG" alt="...">
-            <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
-
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-            <img src="images/tumb.PNG" alt="...">
-            <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
-
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-            <img src="images/tumb.PNG" alt="...">
-            <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
-
-            </div>
-        </div>
-    </div>
-</div>
-
 <div class=".col-xs-6 .col-md-4">
     <div class="pull-right">
         <div class="thumbnail">
